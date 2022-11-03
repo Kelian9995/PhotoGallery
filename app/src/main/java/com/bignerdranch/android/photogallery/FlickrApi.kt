6cb041da44f6385e5f00b7898dd08e2d6a4c0181
Kelian9995 <nsokovnin@gmail.com>
2022-11-03 18:10:13 +0300

    Part 24.JSON -> GSON
    Add RecyclerView

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrApi.kt
@@ -6,6 +6,15 @@ import retrofit2.http.GET
 
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "services/rest/? method=flickr.interestingness.getList" +
+                "&api_key=860d5d1776612833216835622f893480" +
+                "&format=json"+
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+
+
+    )
+
+    fun fetchPhotos(): Call<FlickrResponse>
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -22,6 +24,7 @@ private const val TAG = "PhotoGalleryFragment"
 
 
 class PhotoGalleryFragment : Fragment() {
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,16 +51,18 @@ class PhotoGalleryFragment : Fragment() {
         })
 
         */
-        val flickrLiveData: LiveData<String> =
-            FlickrFetchr().fetchContents()
+        /*val flickrLiveData: LiveData<List<GalleryItem>> =
+            FlickrFetchr().fetchPhotos()
         flickrLiveData.observe(
             this,
             Observer {
-                responseString ->
-                Log.d(TAG, "Response received: $responseString")
-            }
-        )
+                galleryItems ->
+                Log.d(TAG, "Response received: $galleryItems")
+            } )
 
+         */
+
+        photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
     }
 
     override fun onCreateView (
@@ -71,7 +76,38 @@ class PhotoGalleryFragment : Fragment() {
         photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
         return view
 
-    } companion object {
+    }
+    override fun onViewCreated(view:View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        photoGalleryViewModel.galleryItemLiveData.observe(
+            viewLifecycleOwner,
+            Observer {
+                galleryItems ->
+                //Log.d(TAG, "Have gallery items from ViewModel $galleryItems")
+                //Обновить данные поддерживающие представление утилизатора
+                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
+            }
+        )
+    }
+    private class PhotoHolder(itemTextView: TextView):RecyclerView.ViewHolder(itemTextView)
+    {
+        val bindTitle:(CharSequence) -> Unit = itemTextView::setText
+    }
+
+    private class PhotoAdapter(private val galleryItems: List<GalleryItem>)
+        :RecyclerView.Adapter<PhotoHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+            val textView=TextView(parent.context)
+            return PhotoHolder(textView)
+        }
+        override fun getItemCount():Int = galleryItems.size
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem = galleryItems[position]
+            holder.bindTitle(galleryItem.title)
+        }
+    }
+
+    companion object {
         fun newInstance() = PhotoGalleryFragment()
     }
 }
